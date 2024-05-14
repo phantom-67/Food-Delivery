0577e59107f6b408e1331ef7e19a6d80b7d28f85
phantom-67 <swapnil67priyadarshi@gmail.com>
2024-05-14 20:00:36 +0530

    9000

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const port = 4000
 const Mongodb = require("./db")
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000")
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000", "Access-Control-Allow-Origin", "https://phantom-67.github.io");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
